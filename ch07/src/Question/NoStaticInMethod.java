9c3a4be256aac5d31aa32d09597273e306abf76c
이수진 <sujin0970@naver.com>
2023-10-30 23:41:59 +0900

    feat: ch07 객체지향 question code add

---
+++ ch07/src/Question/NoStaticInMethod.java
@@ -0,0 +1,12 @@
+package Question;
+
+class NoStaticInMethod {
+	void method1() {
+//		static int a = 5;	// 컴파일 에러. 메서드 내에 static 선언 불가
+	}
+	
+	public static void main(String args[]) {
+		NoStaticInMethod test = new NoStaticInMethod();
+		test.method1();
+	}
+}
---
+++ ch07/src/Question/StaticMethodNotOveridingCompareVerStatic.java
@@ -0,0 +1,16 @@
+package Question;
+
+class Super {
+    static String greeting() { return "Goodnight"; }
+    String name() { return "Richard"; }
+}
+class Sub extends Super {
+    static String greeting() { return "Hello"; }
+    String name() { return "Dick"; }
+}
+class StaticMethodNotOveridingCompareVerStatic {
+    public static void main(String[] args) {
+        Super s = new Sub();
+        System.out.println(s.greeting() + ", " + s.name());	// Goodnight, Dick
+    }
+}
